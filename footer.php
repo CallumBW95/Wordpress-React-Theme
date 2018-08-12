667c6fc3d2e8d90cbd7fd768e5cc9f30e1640f69
Callum Bridgford-Whittick <callumbw@gmail.com>
2018-08-12 23:20:56 +0100

    Tidy up redundant files

--- footer.php
+++
@@ -1,4 +0,0 @@
-
-  <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/app.js'; ?>"></script>
-  </body>
-</html>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php get_header(); ?>
-<div id="app"></div>
-<?php get_footer(); ?>
+<html>
+  <head>
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+  </head>
+  <body>
+    <div id="app"></div>
+    <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/app.js'; ?>"></script>
+  </body>
+</html>
